a7ade751e914602bba17407c0a8e96c801a00da4
Beat Küng <beat-kueng@gmx.net>
2010-07-11 12:23:55 +0200

    + GPL v2 LICENSE file, licence in file header

--- logging.cpp
+++ logging.cpp
@@ -1,8 +1,15 @@
 /*
- * logging.cpp
+ * Copyright (C) 2010 Beat Küng <beat-kueng@gmx.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  Created on: Apr 28, 2010
- *      Author: beat
  */
 
 #include "logging.h"
